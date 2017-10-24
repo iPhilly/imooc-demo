fef1575aa946cc825686c606b9ff8677fe9afe55
iPhilly <liufei.hehe@qq.com>
2017-10-24 18:15:04 +0800

    layout writing ...

---
+++ src/components/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="app-header">header</div>
+    <div class="app-content">content</div>
+    <div class="app-footer">footer</div>
+    <h1>{{msg}}</h1>
+    <button @click="addOne">add one</button>
+    <button @click="minusOne">minus one</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        msg: 'i am apple',
+        price: 5
+      }
+    },
+    methods: {
+      addOne() {
+
+      },
+      minusOne() {
+
+      }
+    }
+
+  }
+</script>
